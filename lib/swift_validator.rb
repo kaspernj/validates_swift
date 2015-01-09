1e976b526933dda9d2c95803d0dba3501cb9bef4
kaspernj <k@spernj.org>
2015-01-09 15:19:47 +0100

    Remove blank check since that should be handeled by allow_blank

--- lib/swift_validator.rb
+++ lib/swift_validator.rb
@@ -2,13 +2,11 @@ require_relative "swift-code-tool/lib/swift-code"
 
 class SwiftValidator < ActiveModel::EachValidator
   def validate_each(object, attribute, value)
-    if value.present?
-      swift = SWIFTCodeTool::SWIFT.new(value)
-      errors = swift.validation_errors(nil)
+    swift = SWIFTCodeTool::SWIFT.new(value.to_s)
+    errors = swift.validation_errors(nil)
 
-      unless errors.empty?
-        object.errors[attribute] << (options[:message] || "is not a valid SWIFT code: #{errors.join(", ")}")
-      end
+    unless errors.empty?
+      object.errors[attribute] << (options[:message] || "is not a valid SWIFT code: #{errors.join(", ")}")
     end
   end
 end
